f86b834606a96d3a0ee969cc4c13739dd02b974b
James J Balamuta <coatless@users.noreply.github.com>
2023-03-10 20:33:34 -0800

    Update webr.lua

--- _extensions/webr/webr.lua
+++ _extensions/webr/webr.lua
@@ -1,13 +1,23 @@
+-- Define a variable to only include the initialization once
 local hasDoneWebRSetup = false
+-- Define a counter variable
+local counter = 0
 
 -- Setup WebR's pre-requisites once per document.
 function ensureShinyliveSetup()
+  
+  -- If we've included the initialization, then bail.
   if hasDoneWebRSetup then
     return
   end
+  
+  -- Otherwise, let's include the initialization script.
   hasDoneWebRSetup = true
   
   -- Insert the web initialization
   -- https://quarto.org/docs/extensions/lua-api.html#includes
   quarto.doc.include_file("in-header", "partials/webr-init.html")
 end
+
+
+
