1de026aab0eb7d5f4ae0cfd44dd9f739e8be18ab
James J Balamuta <coatless@users.noreply.github.com>
2023-08-17 11:38:08 +0000

    Add a short shell script to update the devcontainer workspace with the latest quarto version. (not high tech :/)

---
+++ update-quarto-dev.sh
@@ -0,0 +1,7 @@
+QUARTO_VERSION="1.4.322"
+
+wget -O quarto-latest.deb https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
+
+sudo dpkg -i ./quarto-latest.deb
+
+rm quarto-latest.deb
