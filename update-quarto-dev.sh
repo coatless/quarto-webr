d961ba7b75d74772af011d2ba0a828bf02ecdcc3
James J Balamuta <coatless@users.noreply.github.com>
2023-09-14 08:00:29 +0000

    Bump development version

--- update-quarto-dev.sh
+++ update-quarto-dev.sh
@@ -1,7 +1,8 @@
-QUARTO_VERSION="1.4.349"
+QUARTO_VERSION="1.4.358"
 
 wget -O quarto-latest.deb https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
 
 sudo dpkg -i ./quarto-latest.deb
 
 rm quarto-latest.deb
+
