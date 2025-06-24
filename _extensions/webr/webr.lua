ec5ce556bb6ddbf26c543c259d9894d874334da9
james.balamuta@gmail.com <james.balamuta@gmail.com>
2025-06-24 13:30:30 -0700

    Bump version of webR to 0.5.2

--- _extensions/webr/webr.lua
+++ _extensions/webr/webr.lua
@@ -15,7 +15,7 @@ local hasDoneWebRSetup = false
 
 --- Define a base compatibile version
 ---@type string
-local baseVersionWebR = "0.4.0"
+local baseVersionWebR = "0.5.2"
 
 --- Define where webR can be found
 ---@type string
